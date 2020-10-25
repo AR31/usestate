b0c469b78b6d80a0f333b0d4e7bd471d4e1ccddb
AR31 <arwa.khlifi31@gmail.com>
2020-10-25 14:03:45 +0100

    add setIntervall

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ProfilPhotoImg from "./ProfilePhoto.jpg";
+class App extends React.Component {
+  state = {
+    fullName: "arwa khifi",
+    bio: "full stack js developer",
+    imgSrc: ProfilPhotoImg,
+    profession: "developer",
+    show: false,
+    date: 0,
+  };
+  handleClick = () => this.setState({ show: true });
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount() {
+    setInterval(() => {
+      var dateTime = new Date();
+      dateTime = dateTime.getHours() + ":" + dateTime.getMinutes();
+      this.setState({ date: dateTime });
+    }, 1000);
+  }
+  render() {
+    return (
+      <>
+        {this.state.show && (
+          <>
+            <p>name: {this.state.fullName}</p>
+            <p>bio: {this.state.bio}</p>
+            <img
+              src={this.state.imgSrc}
+              style={{ width: 300, height: 200 }}
+            ></img>
+            <p>profession: {this.state.profession}</p>
+            <p>{this.state.date}</p>
+          </>
+        )}
+
+        <button onClick={this.handleClick}>update state</button>
+      </>
+    );
+  }
 }
 
 export default App;
